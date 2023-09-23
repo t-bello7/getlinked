ce408cee36a7765cfcd14f9a5b86fbd6721ee4d1
tbello <bello4aus@gmail.com>
2023-09-23 08:52:04 +0100

    ui: style navbar in mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { gsap } from 'gsap';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
 import Button from './atoms/Button';
 import MenuIcon from '../assets/icons/menu-icon.svg'
+import CloseIcon from "../assets/icons/close-icon.svg"
 
 const navlinks = [
   {
@@ -13,20 +14,20 @@ const navlinks = [
 ]
 const Navbar = ({scrollRoutes}) => {
   const [navOpen, setNavOpen] = useState(false);
-  const navRef = useRef(null)
   const handleScroll = (event) => {
+    setNavOpen(!navOpen)
     gsap.registerPlugin(ScrollToPlugin)
     gsap.to(window, {duration: 1, scrollTo: scrollRoutes[event.target.dataset.id].current});  
   }
   return (
-    <div className={`bg-transparent border-b border-b-lightPurple p-5 z-10 fixed container mx-auto flex justify-between items-center md:justify-start md:gap-10`}>
+    <div className={`bg-transparent border-b border-b-lightPurple p-7 z-10 fixed container mx-auto flex justify-between items-center md:justify-start md:gap-10`}>
       <div className="font-clashDisplay font-bold">
         <span className='font-bold text-white'>get</span><span className='text-lightPurple'>linked</span>
       </div>
-      <div className="navlinks flex items-center gap-12 md:w-full">
-        <Button variant='icon' iconPath={MenuIcon} onClick={() => setNavOpen(!navOpen)}/>
-        <div className={navOpen? "h-fit p-5 shadow-md absolute top-0 left-0 w-full" : "hidden md:flex md:w-full md:justify-between"}>
-          <ul className='h-fit md:flex md:gap-4'>
+      <Button variant='icon' iconPath={MenuIcon} onClick={() => setNavOpen(!navOpen)}/>
+      <div className={`h-[60vh] p-12 absolute left-[50%] translate-x-[-50%] w-[90%] rounded-lg bg-darkBlue ${navOpen ? 'top-0' :'-top-[1000%]'}`} >
+          <Button variant='icon' iconPath={CloseIcon} onClick={() => setNavOpen(!navOpen)} extraStyle="float-right"/>
+          <ul className='h-fit my-12 md:flex md:gap-4'>
             <li className='mb-2 pt-2 pb-2' data-id="timeline" onClick={handleScroll}> Timeline </li>
             <li className='mb-2 pt-2 pb-2' data-id="overview" onClick={handleScroll}> Overview </li>
             <li className='mb-2 pt-2 pb-2' data-id="faq" onClick={handleScroll}> FAQs </li>
@@ -37,10 +38,9 @@ const Navbar = ({scrollRoutes}) => {
               </li>
               ))
             }
+            <Button name="Register"/>
           </ul>
-          <Button name="Register"/>
         </div>
-      </div>
     </div>
   );
 }
--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -4,20 +4,20 @@ const Button = ({
     name,
     variant="button",
     iconPath="",
-    defaultStyle="cursor-pointer",
+    defaultStyle="cursor-pointer hover:outline",
     extraStyle,
     onClick
     }) => {
         switch (variant) {
             case "icon":
             return (
-            <button className={`${defaultStyle} p-2 rounded-sm z-50 md:hidden`} onClick={onClick}>
-                <img src={iconPath} alt={`${name}-icon`}/>
+            <button className={`p-2 rounded-sm md:hidden ${defaultStyle} ${extraStyle}`} onClick={onClick}>
+                <img src={iconPath} alt={`${name}-icon`} className="hover:scale-125"/>
             </button>)
 
             default:
                 return (
-            <button className={`bg-${bgColor} text-${color} py-2 px-4 rounded hover:bg-blue-300 capitalize ${defaultStyle} ${extraStyle}`} onClick={onClick}>
+            <button className={`py-2 px-4 rounded hover:bg-blue-300 capitalize bg-lightPurple text-${color}  ${defaultStyle} ${extraStyle}`} onClick={onClick}>
                 {name}
             </button>
         )
